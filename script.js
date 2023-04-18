c3bbd89080ede5771bc8a0717769db3fa53c195e
leonardo Sanchez <leoasr12@hotmail.com>
2023-04-18 17:39:40 -0500

    javascript para numeros

---
+++ script.js
@@ -0,0 +1,34 @@
+// Obtiene los elementos span por su ID
+var numero1 = document.getElementById("numero1");
+var numero2 = document.getElementById("numero2");
+var numero3 = document.getElementById("numero3");
+var numero4 = document.getElementById("numero4");
+
+// Configura los valores iniciales de los números
+var valorInicial = 0;
+var duracion = 500; // 3 segundos
+
+// Función para animar el valor de un número
+function animarNumero(numero, inicio, fin, duracion) {
+  var diferencia = fin - inicio;
+  var incremento = diferencia / duracion * 100;
+  var tiempo = 0;
+  function actualizarNumero() {
+    tiempo += 10;
+    var nuevoValor = inicio + incremento * tiempo / 100;
+    numero.innerHTML = Math.round(nuevoValor);
+    if (tiempo < duracion) {
+      setTimeout(actualizarNumero, 10);
+    } else {
+      numero.innerHTML = fin;
+    }
+  }
+  actualizarNumero();
+}
+
+// Animar los tres números ...animarNumero(numerooo2, valorInicial, valorFinal, duracion);
+
+animarNumero(numero1,valorInicial , 32, duracion);
+animarNumero(numero2, valorInicial, 389, duracion);
+animarNumero(numero3, valorInicial, 500, duracion);
+animarNumero(numero4, valorInicial, 5, duracion);
